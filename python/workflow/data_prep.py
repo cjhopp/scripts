1a65e1ad33efa2c7f3854fa3f22f96b10168c004
cjhopp <cjhopp@mtu.edu>
2017-01-05 14:03:12 -0500

    adding wrapper on template_gen() for pyasdf dataset

--- python/workflow/data_prep.py
+++ python/workflow/data_prep.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 
+def date_generator(start_date, end_date):
+    # Generator for date looping
+    from datetime import timedelta
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
 def sc3ml2qml(zipdir, outdir, stylesheet, prog='xalan'):
     """
     Converting Steve's zipped sc3ml to individual-event qml files
@@ -95,3 +102,86 @@ def asdf_create(asdf_name, wav_dirs, sta_dir):
         for filename in sta_files:
             ds.add_stationxml(filename)
     return
+
+
+def pyasdf_2_templates(asdf_file, cat, outdir, length, prepick,
+                       highcut=None, lowcut=None, f_order=None,
+                       samp_rate=None):
+    """
+    Function to generate individual mseed files for each event in a catalog
+    from a pyasdf file of continuous data.
+    :param asdf_file: ASDF file with waveforms and stations
+    :param cat: Catalog of events for which we'll create templates
+    :param outdir: output directory for miniseed files
+    :param length: length of templates in seconds
+    :param prepick: prepick time for waveform trimming
+    :param highcut: Filter highcut (if desired)
+    :param lowcut: Filter lowcut (if desired)
+    :param f_order: Filter order
+    :param samp_rate: Sampling rate for the templates
+    :return:
+    """
+    import pyasdf
+    import copy
+    from obspy import UTCDateTime, Stream
+    from eqcorrscan.core.template_gen import template_gen
+    from eqcorrscan.utils import pre_processing
+    from timeit import default_timer as timer
+
+    # Establish date range for template creation
+    cat.events.sort(key=lambda x: x.preferred_origin().time)
+    cat_start = cat[0].origins[-1].time.datetime
+    cat_end = cat[-1].origins[-1].time.datetime
+
+    for date in date_generator(cat_start, cat_end):
+        dto = UTCDateTime(date)
+        print('Processing templates for: %s' % str(dto))
+        q_start = dto - 10
+        q_end = dto + 86410
+        # Establish which events are in this day
+        sch_str_start = 'time >= %s' % str(dto)
+        sch_str_end = 'time <= %s' % str(dto + 86400)
+        tmp_cat = cat.filter(sch_str_start, sch_str_end)
+        if len(tmp_cat) == 0:
+            print('No events on: %s' % str(dto))
+            continue
+        # Which stachans we got?
+        stachans = {pk.waveform_id.station_code: [] for ev in tmp_cat
+                    for pk in ev.picks}
+        for ev in tmp_cat:
+            for pk in ev.picks:
+                chan_code = pk.waveform_id.channel_code
+                if chan_code not in stachans[pk.waveform_id.station_code]:
+                    stachans[pk.waveform_id.station_code].append(chan_code)
+        wav_read_start = timer()
+        # Be sure to go +/- 10 sec to account for GeoNet shit timing
+        with pyasdf.ASDFDataSet(asdf_file) as ds:
+            st = Stream()
+            for sta, chans in stachans.iteritems():
+                for station in ds.ifilter(ds.q.station == sta,
+                                          ds.q.channel == chans,
+                                          ds.q.starttime >= q_start,
+                                          ds.q.endtime <= q_end):
+                    st += station.raw_recording
+        wav_read_stop = timer()
+        print('Reading waveforms took %.3f seconds' % (wav_read_stop
+                                                       - wav_read_start))
+        print('Merging stream and preprocessing...')
+        st.merge(fill_value='interpolate')
+        # Cut the stream to a manageable size
+        st1 = pre_processing.dayproc(st, lowcut=lowcut, highcut=highcut,
+                                     filt_order=f_order, samp_rate=samp_rate,
+                                     starttime=dto, debug=debug, parallel=True)
+        print('Feeding stream to _template_gen...')
+        for event in tmp_cat:
+            print('Copying stream to keep away from the trim...')
+            trim_st = copy.deepcopy(st1)
+            ev_name = str(event.resource_id).split('/')[-1]
+            template = template_gen(event.picks, trim_st, length=length,
+                                    prepick=prepick)
+            # temp_list.append(template)
+            print('Writing event %s to file...' % ev_name)
+            template.write('%s/%s_raw.mseed' % (outdir, ev_name),
+                           format="MSEED")
+            del trim_st
+        del tmp_cat, st1, st
