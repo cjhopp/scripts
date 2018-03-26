704545d250840c98351e1b968e03b0b73a93deb2
cjhopp <cjhopp@mtu.edu>
2018-03-26 15:23:00 +1300

    trying to debug all positive relative pols

--- python/workflow/shelly_focmecs.py
+++ python/workflow/shelly_focmecs.py
@@ -88,18 +88,10 @@ def _rel_polarity(data1, data2, min_cc, debug=0):
         return 0.0
     elif np.abs(ccc[raw_max]) < min_cc:
         if debug > 1:
-            print('Correlation below threshold. Skipping.')
+            print('Max absolute correlation below threshold. Skipping.')
         return 0.0
-    if ccc[raw_max] < 0:
-        sign = -1.0
-    else:
-        sign = 1.0
-    # sign = np.sign(ccc[raw_max])
-    if debug > 0:
-        max_neg = np.max(np.abs(ccc[np.where(ccc < 0.)]))
-        print('Sign: {}'.format(sign))
-        print('Max neg: {}'.format(max_neg))
-        print('Max pos: {}'.format(ccc[raw_max]))
+    # Sign of max abs corr
+    sign = np.sign(ccc[raw_max])
     # Find pks
     pk_locs = argrelmax(np.abs(ccc), order=2)[0]
     # Make sure theres more than one peak
