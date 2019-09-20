492a19c9c9cde61a4d929652327bf272731422cd
cjhopp <chopp@lbl.gov>
2019-09-20 16:26:41 -0700

    start catalog creation

--- python/lbnl/events.py
+++ python/lbnl/events.py
@@ -4,3 +4,52 @@
 Functions for the processing, reading, converting of events and event files
 """
 
+from obspy import UTCDateTime, Catalog
+from obspy.core.util import AttribDict
+from obspy.core.event import Pick, Origin, Arrival, Event, Magnitude
+from lbnl.coordinates import SURF_converter
+
+
+def surf_events_to_cat(loc_file, pick_file):
+    """
+    Take location files (hypoinverse formatted) and picks (format TBD)
+    and creates a single obspy catalog for later use and dissemination.
+
+    :param loc_file:
+    :param pick_file:
+    :return:
+    """
+    # Read/parse location file and create Events for each
+    surf_cat = Catalog()
+    with open(loc_file, 'r') as f:
+        next(f)
+        for ln in f:
+            ln = ln.strip('\n')
+            line = ln.split(',')
+            print(line)
+            ot = UTCDateTime(line[1])
+            hmc_east = float(line[2])
+            hmc_north = float(line[3])
+            hmc_elev = float(line[4])
+            converter = SURF_converter()
+            lon, lat, elev = converter.to_lonlat((hmc_east, hmc_north,
+                                                  hmc_elev))
+            o = Origin(time=ot, longitude=lon, latitude=lat, depth=elev)
+            extra = AttribDict({
+                'hmc_east': {
+                    'value': hmc_east,
+                    'namespace': 'smi:local/hmc'
+                },
+                'hmc_north': {
+                    'value': hmc_north,
+                    'namespace': 'smi:local/hmc'
+                },
+                'hmc_elev': {
+                    'value': hmc_elev,
+                    'namespace': 'smi:local/hmc'
+                }
+            })
+            o.extra = extra
+            ev = Event(origins=[o], magnitudes=[Magnitude(mag=1.)])
+            surf_cat.append(ev)
+    return surf_cat
