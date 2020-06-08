939a49645522913b5f55c4b50330915b252a011e
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-08 15:03:47 -0700

    hanging on lowcut=None?

--- python/lbnl/waveforms.py
+++ python/lbnl/waveforms.py
@@ -95,13 +95,12 @@ def downsample_mseeds(wavs, samp_rate, outdir):
         print('Reading {}'.format(w))
         tmp_st = read(w)
         starttime = tmp_st[0].stats.starttime.date
-        print('Merging')
         tmp_st.merge()
         try:
             print('Processing')
             down_st = dayproc(
                 st=tmp_st, samp_rate=samp_rate, starttime=starttime,
-                lowcut=None, highcut=0.4, filt_order=3,
+                lowcut=0., highcut=0.4, filt_order=3, num_cores=len(tmp_st),
                 ignore_length=False, ignore_bad_data=False)
             print('Writing {}'.format(new_name))
             down_st.write(''.format(outdir, new_name), format="MSEED")
