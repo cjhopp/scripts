276d124eb2e50b02738d016f9e1b50297c87c100
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-08 14:42:53 -0700

    fix up downsample mseeds

--- python/lbnl/waveforms.py
+++ python/lbnl/waveforms.py
@@ -93,10 +93,13 @@ def downsample_mseeds(wavs, samp_rate):
     for w in wavs:
         print(w)
         tmp_st = read(w)
+        starttime = tmp_st[0].stats.starttime.date
+        tmp_st.merge()
         try:
-            st += tmp_st.resample(samp_rate)
-        except ZeroDivisionError as e:
-            print('No data in this trace?')
+            st += dayproc(st=tmp_st, samp_rate=samp_rate, starttime=starttime,
+                          lowcut=0.0, highcut=0.4, filt_order=3,
+                          ignore_length=False, ignore_bad_data=False)
+        except ValueError as e:
             print(e)
             continue
     return st
