9b37d1ec903e76037c429baf2eff163e83cb3542
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-08 11:10:27 -0700

    correct pick_time

--- python/lbnl/catalog_build.py
+++ python/lbnl/catalog_build.py
@@ -313,7 +313,7 @@ def plot_picks(st, ev, prepick, postpick, name, outdir):
         tr_raw = st_slice.select(id=sid)[0]
         time_vect = np.arange(time_v.shape[0]) * tr_raw.stats.delta
         ax[i].plot(time_vect, tr_raw.data / np.max(tr_raw.data), color='k')
-        ax[i].axvline(pk_time, linestyle='-', color='r')
+        ax[i].axvline(pk_time[0], linestyle='-', color='r')
         bbox_props = dict(boxstyle="round,pad=0.2", fc="white",
                           ec="k", lw=1)
         ax[i].annotate(s=sid, xy=(0.0, 0.8), xycoords='axes fraction',
