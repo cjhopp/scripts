c1ffebb67a36b3386fc34145ef9eb681dfa51e7b
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-08 17:07:46 -0700

    moving to 3C downsample

--- python/lbnl/waveforms.py
+++ python/lbnl/waveforms.py
@@ -92,7 +92,7 @@ def downsample_mseeds(wavs, samp_rate, start, end, outdir):
     st = Stream()
     for date in date_generator(start, end):
         wavs = [w for w in wavs if str(UTCDateTime(date).julday) in w
-                and str(date.year) in w]
+                and str(UTCDateTime(date).year) in w]
         wavs.sort()
         tmp_st = Stream()
         for w in wavs:
