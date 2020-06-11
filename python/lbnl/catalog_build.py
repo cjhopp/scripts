3a4f16d82471b685b354b04b2a7f50e908c11ea8
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-11 10:04:40 -0700

    avoid events with only sidney stations

--- python/lbnl/catalog_build.py
+++ python/lbnl/catalog_build.py
@@ -174,8 +174,9 @@ def trigger(param_file, plot=False):
             max_trigger_length=trig_p['max_trigger_length'],
             details=True, trigger_off_extension=trig_p['trigger_off_extension'])
         # Enforce at least 5 non-sidney stations (which don't add to locs)
-        day_trigs = [t for t in day_trigs if len([sta for sta in t.stations
-                                              if sta not in sidney_stas]) > 4]
+        day_trigs = [t for t in day_trigs
+                     if len([sta for sta in t['stations']
+                             if sta not in sidney_stas]) > 4]
         if plot:
             plot_triggers(day_trigs, st, trigger_stream,
                           sta_lta_params, network_sta_lta,
