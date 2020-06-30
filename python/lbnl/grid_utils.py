bbe708b9fe523e2410c39f4136363b0b824195a8
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-30 10:26:17 -0700

    savetxt arg is fmt not format

--- python/lbnl/grid_utils.py
+++ python/lbnl/grid_utils.py
@@ -46,21 +46,21 @@ def write_simul2000(dataset, outfile):
         f.write('{},{},{},{}\n'.format(1.0, vp.coords['Easting'].size,
                                        vp.coords['Northing'].size,
                                        vp.coords['depth'].size))
-        np.savetxt(lon, delimiter=',', newline='\n', format='{:0.4f}')
-        np.savetxt(lat, delimiter=',', newline='\n', format='{:0.4f}')
+        np.savetxt(lon, delimiter=',', newline='\n', fmt='{:0.4f}')
+        np.savetxt(lat, delimiter=',', newline='\n', fmt='{:0.4f}')
         np.savetxt(vp.coords['depth'].values, delimiter=',',
-                   newline='\n', format='{:0.4f}')
+                   newline='\n', fmt='{:0.4f}')
         f.write('0,0,0\n0,0,0\n')  # Whatever these are...
         for z in vp.coords['depth'].values:
             for y in lat:
                 np.savetxt(vp.isel(depth=z, Northing=y)['Vp'] / 1000.,
-                           delimiter=',', newline='\n', format='{:0.3f}')
+                           delimiter=',', newline='\n', fmt='{:0.3f}')
         # Finally Vp/Vs ratio
         for z in vp.coords['depth'].values:
             for y in lat:
                 np.savetxt(vp.isel(depth=z, Northing=y) /
                            vs.isel(depth=z, Northing=y),
-                           delimiter=',', newline='\n', format='{:0.3f}')
+                           delimiter=',', newline='\n', fmt='{:0.3f}')
     return
 
 
