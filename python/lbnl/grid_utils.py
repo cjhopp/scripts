0c0d9e8d586303d10d8d4d5384d9572779f6377e
cjhopp <chet.hopp@vuw.ac.nz>
2020-06-30 10:32:10 -0700

    numpy array to savetxt

--- python/lbnl/grid_utils.py
+++ python/lbnl/grid_utils.py
@@ -53,7 +53,7 @@ def write_simul2000(dataset, outfile):
         f.write('0,0,0\n0,0,0\n')  # Whatever these are...
         for z in vp.coords['depth'].values:
             for y in lat:
-                np.savetxt(f, vp.isel(depth=z, Northing=y)['Vp'] / 1000.,
+                np.savetxt(f, vp.isel(depth=z, Northing=y)['Vp'].values / 1000.,
                            delimiter=',', newline='\n', fmt='%0.3f')
         # Finally Vp/Vs ratio
         for z in vp.coords['depth'].values:
