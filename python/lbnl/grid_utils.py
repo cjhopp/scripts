ab2c07a2bc73d582492c52024b5fdca2b2271958
cjhopp <chet.hopp@vuw.ac.nz>
2020-07-01 15:47:43 -0700

    eliminate infs

--- python/lbnl/grid_utils.py
+++ python/lbnl/grid_utils.py
@@ -63,8 +63,9 @@ def write_simul2000(dataset, outfile):
             for j, y in enumerate(vp.coords['Northing']):
                 row_vals = (vp.isel(depth=i, Northing=j) /
                             vs.isel(depth=i, Northing=j)).values[::-1]
-                np.savetxt(f, row_vals.reshape(1, row_vals.shape[0]),
-                           fmt='%5.2f')
+                row_vals = row_vals.reshape(1, row_vals.shape[0])
+                row_vals[row_vals == np.inf] = 0
+                np.savetxt(f, row_vals, fmt='%5.2f')
     return
 
 
