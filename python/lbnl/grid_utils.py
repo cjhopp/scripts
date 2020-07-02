c6340f4ed24a523bd537515558a2866ef6c1f85d
cjhopp <chet.hopp@vuw.ac.nz>
2020-07-02 10:00:03 -0700

    fixing new coordinates

--- python/lbnl/grid_utils.py
+++ python/lbnl/grid_utils.py
@@ -43,20 +43,20 @@ def write_simul2000(dataset, outfile):
     new_dc_p[0] = -2000
     new_dc_p[-1] = 600000
     new_east_p = vp.coords['Easting'].values
-    new_east_p[0] = -100000
-    new_east_p[-1] = 900000
+    new_east_p[0] = -100800
+    new_east_p[-1] = 900800
     new_north_p = vp.coords['Northing'].values
-    new_north_p[0] = 4800000
-    new_north_p[-1] = 5800000
+    new_north_p[0] = 4800300
+    new_north_p[-1] = 5800300
     new_dc_s = vs.coords['depth'].values
     new_dc_s[0] = -2000
     new_dc_s[-1] = 600000
     new_east_s = vs.coords['Easting'].values
-    new_east_s[0] = -100000
-    new_east_s[-1] = 900000
+    new_east_s[0] = -100800
+    new_east_s[-1] = 900800
     new_north_s = vs.coords['Northing'].values
-    new_north_s[0] = 4800000
-    new_north_s[-1] = 5800000
+    new_north_s[0] = 4800300
+    new_north_s[-1] = 5800300
     vp.assign_coords(Easting=new_east_p, Northing=new_north_p, depth=new_dc_p)
     vs.assign_coords(Easting=new_east_s, Northing=new_north_s, depth=new_dc_s)
     # With above indexing, SW vertex is: (-126.3779, 46.1593, -2.5)
