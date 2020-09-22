032a79de5cebac1c0d98a266a90a6bb86bcdea59
Chet Hopp <cjhopp@mtu.edu>
2020-09-21 17:31:55 -0700

    collab review work for martin

--- python/lbnl/hydraulic_data.py
+++ python/lbnl/hydraulic_data.py
@@ -9,7 +9,19 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 from nptdms import TdmsFile
 from glob import glob
-from datetime import datetime
+from scipy.io import loadmat
+from datetime import datetime, timedelta
+
+
+
+def datenum_to_datetime(datenums):
+    # Helper to correctly convert matlab datenum to python datetime
+    # SO source:
+    # https://stackoverflow.com/questions/13965740/converting-matlabs-datenum-format-to-python
+    return [datetime.fromordinal(int(d)) +
+            timedelta(days=d % 1) - timedelta(days=366)
+            for d in datenums]
+
 
 def read_collab_hydro(path):
     """
@@ -22,7 +34,7 @@ def read_collab_hydro(path):
     df['dt'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
     df = df.set_index('dt')
     df = df.drop(['date'], axis=1)
-    df = df.drop(['datenum'], axis=1)
+    df = df.drop(['datetime'], axis=1)
     df.rename(columns={'injection_Lpm': 'Flow', 'pressure_MPa': 'Pressure'},
               inplace=True)
     # Resample this shiz to shrink the dataset
@@ -71,6 +83,98 @@ def read_csd_hydro(root_path):
     all_df = all_df.sort_index()
     return all_df
 
+
+def read_martin_hydro_mat(path):
+    """Helper to read-in raw array .mat files customized from Martin's .mat
+    tables (Table objects don't translate to Python)"""
+    # try:
+    df = pd.DataFrame(loadmat(path)['dat_all'],
+                      columns=['datenum', 'Quizix Press', 'Quizix Flow',
+                               'Quizix Volume', 'Triplex Flow', 'SNL16',
+                               'PNNL08', 'SNL10', 'SNL11'])
+    # except KeyError:
+    #     df = pd.DataFrame(loadmat(path)['dat_dec_ALL_array'],
+    #                       columns=['datenum', 'Quizix Press', 'Quizix Flow',
+    #                                'Quizix Volume', 'Triplex Flow', 'SNL16',
+    #                                'PNNL08', 'SNL10', 'SNL11'])
+    t = datenum_to_datetime(df.datenum)
+    t = [t.replace(year=2018) for t in t]
+    df.index = t
+    df = df.drop(labels=['datenum', 'Quizix Volume'], axis=1)
+    return df
+
+
+def martin_cumulative_vols(df_early):
+    """
+    One-time func to generate cumulative volumes and max pressures for each
+    stim in Martin's paper
+
+    :param collab_df: Collab DataFrame
+
+    :return:
+    """
+
+    stims = [
+        (datetime(2018, 5, 22, 21, 40, 00), datetime(2018, 5, 22, 22, 10, 00)),  #Quizix
+        (datetime(2018, 5, 23, 18, 20, 00), datetime(2018, 5, 23, 20, 10, 00)),  #Quizix
+        (datetime(2018, 5, 24, 22, 10, 00), datetime(2018, 5, 24, 23, 00, 00)),  #Triplex
+        (datetime(2018, 5, 25, 15, 00, 00), datetime(2018, 5, 25, 15, 45, 00)),  #Triplex
+        (datetime(2018, 5, 25, 20, 20, 00), datetime(2018, 5, 25, 21, 30, 00)),  #Triplex
+        (datetime(2018, 6, 25, 17, 00, 00), datetime(2018, 6, 25, 18, 30, 00)),  #
+        (datetime(2018, 6, 25, 18, 30, 00), datetime(2018, 6, 25, 21, 30, 00)),  #
+        (datetime(2018, 7, 19, 14, 00, 00), datetime(2018, 7, 19, 22, 00, 00)),  #Quizix
+        (datetime(2018, 7, 20, 14, 30, 00), datetime(2018, 7, 21, 00, 00, 00)),  #Combo
+        (datetime(2018, 12, 7, 23, 00, 00), datetime(2018, 12, 7, 23, 30, 00)),  #Triplex
+        (datetime(2018, 12, 20, 16, 32, 00), datetime(2018, 12, 20, 21, 30, 00)),  #Triplex
+        (datetime(2018, 12, 21, 18, 30, 00), datetime(2018, 12, 21, 23, 30, 00))]  #Triplex
+    for stim in stims:
+        # Stim name
+        name = '{}'.format(stim[0].date())
+        fig, ax = plt.subplots(figsize=(10, 7))
+        ax2 = ax.twinx()
+        stim_df = df_early[stim[0]:stim[1]]
+        # Downsample to 30s
+        stim_df = stim_df.resample('30s').mean()
+        # Eliminate negative values
+        stim_df[stim_df < 0] = 0.
+        # Pressures to MPa
+        if stim[0].month == 6:
+            stim_df[['P1', 'P2']] = (stim_df[['PNNL08',
+                                              'SNL11']] / 145.)
+            # Do cumulative sum, account for L/min measure every 30 s
+            stim_df['Cumulative'] = (
+                    stim_df['SNL16'].cumsum() * 0.5 +
+                    stim_df['Triplex Flow'].cumsum() + 0.5)
+            stim_df[['Quizix Flow', 'Triplex Flow', 'SNL16']].plot(
+                ax=ax, legend=False)
+        else:
+            stim_df[['P1', 'P2']] = (stim_df[['Quizix Press',
+                                              'SNL10']] / 145.)
+            stim_df['Cumulative'] = (
+                    stim_df['Quizix Flow'].cumsum() * 0.5 +
+                    stim_df['Triplex Flow'].cumsum() * 0.5)
+            stim_df[['Quizix Flow', 'Triplex Flow']].plot(
+                ax=ax, legend=False)
+        stim_df['Cumulative'].plot(ax=ax2, color='k')
+        ax.set_title('Stim {} - {}'.format(stim[0], stim[1]))
+        ax.text(x=0.05, y=0.9,
+                s='Max Volume: {:.3f} L\nMax Press: {:0.3f} MPa'.format(
+                    stim_df['Cumulative'].max(),
+                    stim_df[['P1', 'P2']].max(axis=1).max()),
+                transform=ax.transAxes, fontsize=14,
+                bbox=dict(boxstyle="round",
+                          ec='k', fc='white',
+                          zorder=103))
+        ax.set_ylabel('L/min')
+        ax2.set_ylabel('Liters')
+        # Write csv
+        stim_df.to_csv('Stim_{}.csv'.format(name))
+        fig.legend()
+        plt.savefig('Stim_{}.png'.format(name), dpi=200)
+    return
+
+
+###### Plotting #######
 
 def plot_csd_hydro(df_hydro, title='Flow and Pressure'):
     """Simple Flow and Press plot"""
