d04dba49283f06cd2fe95f5c0d26c91e09367522
Chet Hopp <cjhopp@mtu.edu>
2021-02-26 17:11:46 -0800

    handle wierd masked arrays out of EQcorrscan

--- python/lbnl/waveforms.py
+++ python/lbnl/waveforms.py
@@ -575,7 +575,8 @@ def detect_tribe_client(tribe, client, start, end, param_dict,
                         os.makedirs(daylong_dir)
                     # Ensure there are no masked arrays
                     for tr in day_st:
-                        tr.data = tr.data.filled()
+                        print(tr.data)
+                        tr.data = tr.data[tr.data.mask == False]
                     day_st.write('{}/{}.mseed'.format(daylong_dir, date),
                                  format='MSEED')
             else:
