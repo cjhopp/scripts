cb9bd7d6cf68afe54bdef60c1983c332291fa758
cjhopp <chopp@lbl.gov>
2021-06-10 14:45:34 -0600

    check for plots already written

--- python/lbnl/waveforms.py
+++ python/lbnl/waveforms.py
@@ -852,6 +852,12 @@ def party_multiplot_wavdir(party, wav_dir, plotdir, start, end):
         if len(detections) == 0:
             print('No detections')
             continue
+        fns = [os.path.exists('{}/{}.png'.format(plotdir, det[0].id))
+               for det in detections]
+        if all(fns):
+            print('All plots saved. Next')
+            continue
+        detections = [d for d, f in enumerate(zip(detections, fns)) if f]
         net_sta_loc_chans = list(set([(pk.waveform_id.network_code,
                                        pk.waveform_id.station_code,
                                        pk.waveform_id.location_code,
