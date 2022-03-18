635dacc6dd3a2c5a57d300978942b737dce94e8e
cjhopp <chopp@lbl.gov>
2022-03-18 16:09:49 -0600

    DTS deltaT to pm 1

--- python/experiment/Collab_4100_DTS_QC_Stim.py
+++ python/experiment/Collab_4100_DTS_QC_Stim.py
@@ -363,7 +363,7 @@ while True:
             print('Plotting {}'.format(out_c1))
             plot_4100_QC(well_data_c1, well=w, depths=[20, 40],
                          baseline=base_dict[w], outfile=out_c1,
-                         vrange_T=(15, 30), vrange_dT=(-2, 2))
+                         vrange_T=(15, 30), vrange_dT=(-1, 1))
         else:
             print('Already plotted\n{}'.format(out_c1))
     time.sleep(ping_interval_in_seconds)
