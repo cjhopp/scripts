8462e88d413efe920016c5bc8d1a0637c2b3fc74
cjhopp <chopp@lbl.gov>
2022-03-18 15:25:52 -0600

    DTS copy cron script

--- bash/CollabDTS_cron.sh
+++ bash/CollabDTS_cron.sh
@@ -8,5 +8,5 @@ declare -a StringArray=("DMU" "DML" "AMU" "AML" )
 # Iterate the string array using for loop
 for val in ${StringArray[@]}; do
    echo $val
-   scp $(ls -tp . | grep -v / | head -n 4 | grep $val) chet@gmf4.lbl.gov:/var/www/sigmav/web/plots/DTS/$val.png
+   scp $(ls -tp . | grep -v / | head -n 4 | grep "$val") chet@gmf4.lbl.gov:/var/www/sigmav/web/plots/DTS/$val.png
 done
