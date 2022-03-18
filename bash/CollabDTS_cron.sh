950a1ea3673cd53e6ee4e32f2cf8b4d8e527c00b
cjhopp <chopp@lbl.gov>
2022-03-18 15:14:36 -0600

    DTS copy cron script

---
+++ bash/CollabDTS_cron.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /mnt/c/Users/loaner/Google\ Drive/collabDataTransfer/rawDTSplots/Collab4100-stim
+
+# Do each individually
+declare -a StringArray=("DMU" "DML" "AMU" "AML" )
+
+# Iterate the string array using for loop
+for val in ${StringArray[@]}; do
+   echo $val
+   scp $(ls -tp . | grep -v / | head -n 4 | grep $val) chet@gmf4.lbl.gov:/var/www/sigmav/web/plots/DTS/$val.png
+done
