69639cc97fe94d82b3c6aee4acff196d88576722
cjhopp <chopp@lbl.gov>
2022-07-12 15:52:39 -0600

    TM-JV picking

--- python/lbnl/catalog_build.py
+++ python/lbnl/catalog_build.py
@@ -585,11 +585,8 @@ def trigger(param_file, plot=False):
             day_wavs = []
             for seed in trig_p['seeds']:
                 net, sta, loc, chan = seed.split('.')
-                print('{}/{}/{}.{}.{}.{}.{}.{:03d}.ms'.format(
-                        paramz['General']['wav_directory'], sta,
-                        sta, net, loc, chan, date.year, jday))
                 day_wavs.append(
-                    '{}/{}/{}.{}.{}.{}.{}.{:03d}.ms'.format(
+                    '{}/{}/{}.{}.{}.{}.{}.{:03d}'.format(
                         paramz['General']['wav_directory'], sta,
                         sta, net, loc, chan, date.year, jday))
         else:
