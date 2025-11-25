4483b1bebd33654f00d777cf7ca9eacb1b1164df
chopp <chopp@lbl.gov>
2025-11-25 22:30:27 +0000

    Do away with inheritance of WaveformDataset. Do it manually as script

--- python/lbnl/seisbench_util.py
+++ python/lbnl/seisbench_util.py
@@ -27,170 +27,133 @@ from obspy.geodetics import gps2dist_azimuth
 from obspy.clients.fdsn import Client
 
 
-class Cape_downloader(WaveformDataset):
+dataset_root = Path('/media/chopp/HDD1/chet-meq/cape_modern/seisbench/cape_v1/dataset')
+metadata_path = base_path / "metadata.csv"
+waveforms_path = base_path / "waveforms.hdf5"
+
+
+clients = {'6K': Client("http://131.243.224.19:8085"),
+           'SS': Client("http://131.243.224.19:8085"),
+           'UU': Client("IRIS")}
+
+
+
+def get_event_params(event, split):
+    origin = event.preferred_origin()
+    mag = event.preferred_magnitude()
+    fm = event.preferred_focal_mechanism()
+
+
+    if str(event.resource_id).split("/")[-1] == "1":
+        # Generate custom source-id
+        chars = string.ascii_lowercase + string.digits
+        source_id = "sb_id_" + "".join(random.choice(chars) for _ in range(6))
+    else:
+        source_id = str(event.resource_id).split("/")[-1]
+
+
+    event_params = {
+        "source_id": source_id,
+        "source_origin_time": str(origin.time),
+        "source_origin_uncertainty_sec": origin.time_errors["uncertainty"],
+        "source_latitude_deg": origin.latitude,
+        "source_latitude_uncertainty_km": origin.latitude_errors["uncertainty"],
+        "source_longitude_deg": origin.longitude,
+        "source_longitude_uncertainty_km": origin.longitude_errors["uncertainty"],
+        "source_depth_km": origin.depth / 1e3,
+        "source_depth_uncertainty_km": origin.depth_errors["uncertainty"] / 1e3,
+    }
+
+    event_params["split"] = split
+
+    if mag is not None:
+        event_params["source_magnitude"] = mag.mag
+        event_params["source_magnitude_uncertainty"] = mag.mag_errors["uncertainty"]
+        event_params["source_magnitude_type"] = mag.magnitude_type
+        event_params["source_magnitude_author"] = mag.creation_info.agency_id
+
+    if fm is not None:
+        try:
+            t_axis, p_axis, n_axis = (
+                fm.principal_axes.t_axis,
+                fm.principal_axes.p_axis,
+                fm.principal_axes.n_axis,
+            )
+            event_params["source_focal_mechanism_t_azimuth"] = t_axis.azimuth
+            event_params["source_focal_mechanism_t_plunge"] = t_axis.plunge
+            event_params["source_focal_mechanism_t_length"] = t_axis.length
+
+            event_params["source_focal_mechanism_p_azimuth"] = p_axis.azimuth
+            event_params["source_focal_mechanism_p_plunge"] = p_axis.plunge
+            event_params["source_focal_mechanism_p_length"] = p_axis.length
+
+            event_params["source_focal_mechanism_n_azimuth"] = n_axis.azimuth
+            event_params["source_focal_mechanism_n_plunge"] = n_axis.plunge
+            event_params["source_focal_mechanism_n_length"] = n_axis.length
+        except AttributeError:
+            # There seem to be a few broken xml files. In this case, just ignore the focal mechanism.
+            pass
+
+    return origin, mag, fm, event_params
+
+
+def get_trace_params(pick, inventory, event_params):
+    net = pick.waveform_id.network_code
+    sta = pick.waveform_id.station_code
+
+    lat, lon, elev, depth = inventory.get_station_location(network=net, station=sta)
+
+    if not np.isnan(lat * lon):
+        back_azimuth = gps2dist_azimuth(
+            event_params["source_latitude_deg"],
+            event_params["source_longitude_deg"],
+            lat,
+            lon,
+        )[2]
+    else:
+        back_azimuth = np.nan
+
+    trace_params = {
+        "path_back_azimuth_deg": back_azimuth,
+        "station_network_code": net,
+        "station_code": sta,
+        "trace_channel": pick.waveform_id.channel_code[:2],
+        "station_location_code": pick.waveform_id.location_code,
+        "station_latitude_deg": lat,
+        "station_longitude_deg": lon,
+        "station_elevation_m": elev,
+        "station_depth_m": depth
+    }
+
+    return trace_params
+
+
+def download_dataset(catalog, inventory, time_before=10, time_after=10, **kwargs):
     """
-    Regional benchmark dataset of publicly available waveform data & corresponding
-    metadata in Swiss Seismological Service (SED) archive. Contains data from
-    (2013-2020). A pre-compiled version of the benchmark dataset in compatible
-    SeisBench format is available for download from remote root. In case of download issues,
-    benchmark dataset is downloaded directly from source via FDSN client and converted SeisBench
-    format.
+    Download dataset from raw data source via FDSN client.
+
+
+    :param catalog: Catalog of events to download.
+    :type catalog: obspy.core.event.Catalog
+    :param inventory: Inventory of stations.
+    :type inventory: obspy.core.inventory.Inventory
+    :param writer:  WaveformDataWriter instance for writing waveforms and metadata.
+    :type writer: seisbench.data.base.WaveformDataWriter
+    :param time_before: Extract waveform recordings from event onset - time_before, defaults to 60
+    :type time_before: int, optional
+    :param time_after: Extract waveform recordings up to event onset + time_after, defaults to 60
+    :type time_after: int, optional
 
 
     """
-
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._path = '/media/chopp/HDD1/chet-meq/cape_modern/seisbench/testing/dataset/'
-        self._clients = None
-
-
-    @classmethod
-    def _fdsn_clients(cls):
-        return {'6K': Client("http://131.243.224.19:8085"),
-                'SS': Client("http://131.243.224.19:8085"),
-                'UU': Client("IRIS")}
-
-
-    @property
-    def client(self):
-        if self._clients is None:
-            self._clients = self._fdsn_clients()
-        return self._clients
-    
-    @staticmethod
-    def _get_event_params(event):
-        origin = event.preferred_origin()
-        mag = event.preferred_magnitude()
-        fm = event.preferred_focal_mechanism()
-
-
-        if str(event.resource_id).split("/")[-1] == "1":
-            # Generate custom source-id
-            chars = string.ascii_lowercase + string.digits
-            source_id = "sb_id_" + "".join(random.choice(chars) for _ in range(6))
-        else:
-            source_id = str(event.resource_id).split("/")[-1]
-
-
-        event_params = {
-            "source_id": source_id,
-            "source_origin_time": str(origin.time),
-            "source_origin_uncertainty_sec": origin.time_errors["uncertainty"],
-            "source_latitude_deg": origin.latitude,
-            "source_latitude_uncertainty_km": origin.latitude_errors["uncertainty"],
-            "source_longitude_deg": origin.longitude,
-            "source_longitude_uncertainty_km": origin.longitude_errors["uncertainty"],
-            "source_depth_km": origin.depth / 1e3,
-            "source_depth_uncertainty_km": origin.depth_errors["uncertainty"] / 1e3,
-        }
-
-
-        # if str(origin.time) < "2019-01-08":
-        #     split = "train"
-        # elif str(origin.time) < "2019-09-04":
-        #     split = "dev"
-        # else:
-        #     split = "test"
-        # event_params["split"] = split
-
-
-        if mag is not None:
-            event_params["source_magnitude"] = mag.mag
-            event_params["source_magnitude_uncertainty"] = mag.mag_errors["uncertainty"]
-            event_params["source_magnitude_type"] = mag.magnitude_type
-            event_params["source_magnitude_author"] = mag.creation_info.agency_id
-
-
-        if fm is not None:
-            try:
-                t_axis, p_axis, n_axis = (
-                    fm.principal_axes.t_axis,
-                    fm.principal_axes.p_axis,
-                    fm.principal_axes.n_axis,
-                )
-                event_params["source_focal_mechanism_t_azimuth"] = t_axis.azimuth
-                event_params["source_focal_mechanism_t_plunge"] = t_axis.plunge
-                event_params["source_focal_mechanism_t_length"] = t_axis.length
-
-
-                event_params["source_focal_mechanism_p_azimuth"] = p_axis.azimuth
-                event_params["source_focal_mechanism_p_plunge"] = p_axis.plunge
-                event_params["source_focal_mechanism_p_length"] = p_axis.length
-
-
-                event_params["source_focal_mechanism_n_azimuth"] = n_axis.azimuth
-                event_params["source_focal_mechanism_n_plunge"] = n_axis.plunge
-                event_params["source_focal_mechanism_n_length"] = n_axis.length
-            except AttributeError:
-                # There seem to be a few broken xml files. In this case, just ignore the focal mechanism.
-                pass
-
-
-        return origin, mag, fm, event_params
-
-    @staticmethod
-    def _get_trace_params(pick, inventory, event_params):
-        net = pick.waveform_id.network_code
-        sta = pick.waveform_id.station_code
-
-
-        lat, lon, elev, depth = inventory.get_station_location(network=net, station=sta)
-
-
-        if not np.isnan(lat * lon):
-            back_azimuth = gps2dist_azimuth(
-                event_params["source_latitude_deg"],
-                event_params["source_longitude_deg"],
-                lat,
-                lon,
-            )[2]
-        else:
-            back_azimuth = np.nan
-
-
-        trace_params = {
-            "path_back_azimuth_deg": back_azimuth,
-            "station_network_code": net,
-            "station_code": sta,
-            "trace_channel": pick.waveform_id.channel_code[:2],
-            "station_location_code": pick.waveform_id.location_code,
-            "station_latitude_deg": lat,
-            "station_longitude_deg": lon,
-            "station_elevation_m": elev,
-            "station_depth_m": depth
-        }
-
-
-        return trace_params
-
-
-    def download_dataset(self, catalog, inventory, writer, time_before=10, time_after=10, **kwargs):
-        """
-        Download dataset from raw data source via FDSN client.
-
-
-        :param catalog: Catalog of events to download.
-        :type catalog: obspy.core.event.Catalog
-        :param inventory: Inventory of stations.
-        :type inventory: obspy.core.inventory.Inventory
-        :param writer:  WaveformDataWriter instance for writing waveforms and metadata.
-        :type writer: seisbench.data.base.WaveformDataWriter
-        :param time_before: Extract waveform recordings from event onset - time_before, defaults to 60
-        :type time_before: int, optional
-        :param time_after: Extract waveform recordings up to event onset + time_after, defaults to 60
-        :type time_after: int, optional
-
-
-        """
-        seisbench.logger.info(
-            "No pre-processed version of Cape dataset found. "
-            "Download and conversion of raw data will now be "
-            "performed. This may take a while."
-        )
-
-
+    seisbench.logger.info(
+        "No pre-processed version of Cape dataset found. "
+        "Download and conversion of raw data will now be "
+        "performed. This may take a while."
+    )
+    # Iterate over events and picks, write to SeisBench format
+    with sbd.WaveformDataWriter(metadata_path, waveforms_path) as writer:
+        # Define data format
         writer.data_format = {
             "dimension_order": "CW",
             "component_order": "ZNE",
@@ -199,42 +162,87 @@ class Cape_downloader(WaveformDataset):
             "instrument_response": "not restituted",
         }
 
-
-        self.not_in_inv_catches = 0
-        self.no_data_catches = 0
-
-
         for event in catalog:
-            origin, mag, fm, event_params = self._get_event_params(event)
-            seisbench.logger.info(f"Downloading {event.resource_id}")
+            event_params = get_event_params(event)
+            for pick in event.picks:
+                trace_params = get_trace_params(pick)
+                waveforms = get_waveforms(pick, trace_params)
 
+                if len(waveforms) == 0:
+                    # No waveform data available
+                    continue
+
+                sampling_rate = waveforms[0].stats.sampling_rate
+                # Check that the traces have the same sampling rate
+                assert all(
+                    trace.stats.sampling_rate == sampling_rate for trace in waveforms
+                )
+
+                actual_t_start, data, _ = sbu.stream_to_array(
+                    waveforms,
+                    component_order=writer.data_format["component_order"],
+                )
+
+                trace_params["trace_sampling_rate_hz"] = sampling_rate
+                trace_params["trace_start_time"] = str(actual_t_start)
+
+                sample = (pick.time - actual_t_start) * sampling_rate
+                trace_params[f"trace_{pick.phase_hint}_arrival_sample"] = int(sample)
+                trace_params[f"trace_{pick.phase_hint}_status"] = pick.evaluation_mode
+
+                writer.add_trace({**event_params, **trace_params}, data)
+        writer.data_format = {
+            "dimension_order": "CW",
+            "component_order": "ZNE",
+            "measurement": "velocity",
+            "unit": "counts",
+            "instrument_response": "not restituted",
+        }
+
+        not_in_inv_catches = 0
+        no_data_catches = 0
+
+        num_splits = kwargs.get("num_splits", 1)  # Default to 1 if not provided
+
+        # Randomly assign events to splits
+        event_indices = list(range(len(catalog)))
+        random.shuffle(event_indices)
+        
+        # Create an array to store split assignments for each event
+        split_assignments = np.zeros(len(catalog), dtype=int)
+        for i, idx in enumerate(event_indices):
+            split_assignments[idx] = i % num_splits
+
+        for i, event in enumerate(catalog):
+            event_split = split_assignments[i]
+            origin, mag, fm, event_params = get_event_params(event, event_split)
+
+            origin, mag, fm, event_params = get_event_params(event, split)
+            seisbench.logger.info(f"Downloading {event.resource_id}")
 
             station_groups = defaultdict(list)
             for pick in event.picks:
                 if pick.phase_hint is None:
                     continue
 
-
                 station_groups[
                     waveform_id_to_network_station_location(pick.waveform_id.id)
                 ].append(pick)
 
-
             for picks in station_groups.values():
                 try:
-                    trace_params = self._get_trace_params(
+                    trace_params = get_trace_params(
                         picks[0], inventory_mapper, event_params
                     )
                 except KeyError as e:
-                    self.not_in_inv_catches += 1
+                    not_in_inv_catches += 1
                     seisbench.logger.debug(e)
                     continue
-
 
                 t_start = min(pick.time for pick in picks) - time_before
                 t_end = max(pick.time for pick in picks) + time_after
 
-                cli = self.client[trace_params["station_network_code"]]
+                cli = clients[trace_params["station_network_code"]]
                 try:
                     waveforms = cli.get_waveforms(
                         network=trace_params["station_network_code"],
@@ -246,7 +254,7 @@ class Cape_downloader(WaveformDataset):
                     )
                 except FDSNNoDataException as e:
                     seisbench.logger.debug(e)
-                    self.no_data_catches += 1
+                    no_data_catches += 1
                     continue
 
                 # rotate_stream_to_zne(waveforms, inv)
@@ -257,7 +265,6 @@ class Cape_downloader(WaveformDataset):
                         f' in event {event_params["source_id"]}'
                     )
                     continue
-
 
                 sampling_rate = waveforms[0].stats.sampling_rate
                 if any(
@@ -270,20 +277,16 @@ class Cape_downloader(WaveformDataset):
                     )
                     waveforms.resample(sampling_rate)
 
-
                 trace_params[
                     "trace_name"
                 ] = f"{event_params['source_id']}_{waveform_id_to_network_station_location(picks[0].waveform_id.id)}"
 
-
                 stream = waveforms.slice(t_start, t_end)
-
 
                 actual_t_start, data, completeness = stream_to_array(
                     stream,
                     component_order=writer.data_format["component_order"],
                 )
-
 
                 if int((t_end - t_start) * sampling_rate) + 1 > data.shape[1]:
                     # Traces appear to be complete, but do not cover the intended time range
@@ -291,12 +294,10 @@ class Cape_downloader(WaveformDataset):
                         int((t_end - t_start) * sampling_rate) + 1
                     )
 
-
                 trace_params["trace_sampling_rate_hz"] = sampling_rate
                 trace_params["trace_completeness"] = completeness
                 trace_params["trace_has_spikes"] = trace_has_spikes(data)
                 trace_params["trace_start_time"] = str(actual_t_start)
-
 
                 for pick in picks:
                     sample = (pick.time - actual_t_start) * sampling_rate
@@ -315,7 +316,6 @@ class Cape_downloader(WaveformDataset):
                             f"trace_{pick.phase_hint}_polarity"
                         ] = pick.polarity
 
-
                 writer.add_trace({**event_params, **trace_params}, data)
 
 
@@ -323,7 +323,6 @@ class InventoryMapper:
     """
     Helper class to map station inventories to metadata.
     """
-
 
     def __init__(self, inv):
         self.nested_sta_meta = self._create_nested_metadata(inv)
@@ -333,11 +332,9 @@ class InventoryMapper:
     def _create_nested_metadata(inv):
         nested_station_meta = {}
 
-
         for network in inv._networks:
             network_code = network._code
             nested_station_meta[network_code] = {}
-
 
             for station in network.stations:
                 nested_station_meta[network_code][station._code] = {
@@ -348,7 +345,6 @@ class InventoryMapper:
                     "elevation": station._elevation,
                     "depth": station[0].depth
                 }
-
 
         return nested_station_meta
 
@@ -368,3 +364,12 @@ class InventoryMapper:
             )
         except KeyError as e:
             raise KeyError(f"station code '{e.args[0]}' not in inventory")
+
+
+if __name__ == "__main__":
+    from seisbench.io import read_events, read_inventory
+
+    catalog = read_events('/media/chopp/HDD1/chet-meq/cape_modern/catalogs/seiscomp/manually_reviewed/Cape_manually-reviewed_5-10-24.xml')
+    inventory = read_inventory("/media/chopp/HDD1/chet-meq/cape_modern/instruments/Cape_modern_all_inventories_11-25-2025.xml")
+
+    download_dataset(catalog, inventory, num_splits=5) # Assuming num_splits is an argument
